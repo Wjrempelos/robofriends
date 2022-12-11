d0f4fa4ed6d7c299e7302904ed658a221bcde36b
Bill Rempelos <wjrempelos@gmail.com>
2022-12-11 08:21:21 -0700

    added redux to project

--- src/index.js
+++ src/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 // import ReactDOM from 'react-dom'; The new way to import createRoot:
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import 'tachyons';
+import { searchRobots, requestRobots } from './reducers';
 // import registerServiceWorker from './registerServiceWorker';
 
+const logger = createLogger();
+
+const rootReducer = combineReducers({ searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 const root = createRoot(document.getElementById('root'));
-root.render(<App />);
-
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>)
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
